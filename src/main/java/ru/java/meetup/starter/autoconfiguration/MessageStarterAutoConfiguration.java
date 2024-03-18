d8bf43cbe582fb6f6e165595ac09fe6cec829f74
a.shafor <a.shafor@tinkoff.ru>
2024-03-18 13:32:34 +0400

    feature: add autoconfiguration imports

--- src/main/java/ru/java/meetup/starter/autoconfiguration/MessageStarterAutoConfiguration.java
+++ src/main/java/ru/java/meetup/starter/autoconfiguration/MessageStarterAutoConfiguration.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import ru.java.meetup.starter.controller.MessageController;
 import ru.java.meetup.starter.service.MessageService;
 import ru.java.meetup.starter.service.impl.MessageServiceImpl;
 
 @AutoConfiguration
+@Import(MessageController.class)
 public class MessageStarterAutoConfiguration {
 
     @Bean
